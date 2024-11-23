d9deb7efbfc63a0fdf85c96b1e319c244d2fc749
Taufika24 <jtaufika243@gmail.com>
2024-11-23 21:05:30 +0530

    Update vowel_check.c

--- vowel_check.c
+++ vowel_check.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main() {
     char ch;
-    printf("Enter an alphabet: ");
-    scanf("%c", &ch);
+    printf("Enter character");
+    scanf("%c",&ch);
     if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) 
     {
         if (ch >= 'A' && ch <= 'Z')
             ch = ch + 32;
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-            printf("The character is a vowel.\n");
+            printf("Character is a vowel");
         else 
-            printf("The character is a consonant.\n");
+            printf("Character is a consonant");
    else
-        printf("The input is not an alphabet.\n");
+        printf("The input is not an alphabet");
     return 0;
 }
