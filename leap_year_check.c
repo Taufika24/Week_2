e5badb328a69f408efd77de1dacb1836d5021d0f
Taufika24 <jtaufika243@gmail.com>
2024-11-23 20:19:25 +0530

    Create leap_year_check.c

---
+++ leap_year_check.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
+        printf("Given year is a leap year\n");
+     else
+        printf("Given year is not a leap year\n");
+       return 0;
+}
